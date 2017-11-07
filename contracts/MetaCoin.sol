44642a07247d57a2a2dae27ff9ab5b927f66154a
Sunjin Lee <styner32@gmail.com>
2017-11-07 16:19:07 +0800

    updated contracts

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -4,24 +4,32 @@ import "./ConvertLib.sol";
 
 contract MetaCoin {
   struct Post {
+    uint id;
     uint timestamp;
     string content;
+    address createdBy;
+  }
+
+  struct Answer {
+    uint postID;
+    string content;
+    address createdBy;
+    uint timestamp;
   }
 
   uint numOfPosts;
 
   mapping (uint => Post) public posts;
+  mapping (uint => Answer[]) public answers;
   mapping (address => uint) balances;
 
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
   function MetaCoin() {
-    balances[tx.origin] = 10000;
     numOfPosts = 0;
   }
 
   function sendCoin(address receiver, uint amount) returns (bool sufficient) {
-    if (balances[msg.sender] < amount) return false;
     balances[msg.sender] -= amount;
     balances[receiver] += amount;
     Transfer(msg.sender, receiver, amount);
@@ -36,15 +44,18 @@ contract MetaCoin {
     return balances[addr];
   }
 
-  function createPost(string content) public returns (bool sufficient) {
-    posts[numOfPosts].timestamp = now;
+  function createPost(string content) public returns (uint id) {
+    posts[numOfPosts].id = id;
     posts[numOfPosts].content = content;
+    posts[numOfPosts].createdBy = msg.sender;
+    posts[numOfPosts].timestamp = now;
+    id = numOfPosts;
     numOfPosts++;
-    return true;
   }
 
   function getPost(uint postID) public returns (string content, uint timestamp) {
-    return (posts[postID].content, posts[postID].timestamp);
+    content = posts[postID].content;
+    timestamp = posts[postID].timestamp;
   }
 
   function getLatestPost() constant returns (string content, uint timestamp, uint numberOfPosts) {
@@ -53,7 +64,10 @@ contract MetaCoin {
     numberOfPosts = numOfPosts;
   }
 
-  function getNumberOfPosts() public constant returns (uint numOfPosts) {
-    return numOfPosts;
+  function getNumberOfPosts() public constant returns (uint num) {
+    num = numOfPosts;
+  }
+
+  function createAnswer(uint postID, string content) returns (uint id) {
   }
 }
