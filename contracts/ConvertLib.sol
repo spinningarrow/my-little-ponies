9d8b0c1404f4bc39b87daf967f614d2b9471daeb
Sunjin Lee <styner32@gmail.com>
2017-11-07 17:02:43 +0800

    updated contracts

--- contracts/ConvertLib.sol
+++ contracts/ConvertLib.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 
 library ConvertLib{
-  function convert(int amount, int conversionRate) returns (int convertedAmount)
+  function convert(int amount, int conversionRate) constant returns (int convertedAmount)
   {
     return amount * conversionRate;
   }
--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -5,71 +5,81 @@ import "./ConvertLib.sol";
 contract MetaCoin {
   struct Post {
     uint id;
-    uint timestamp;
     string title;
     string content;
+    uint timestamp;
     address createdBy;
   }
 
   struct Answer {
     uint postId;
+    uint id;
     string content;
     address createdBy;
     uint timestamp;
   }
 
   uint numOfPosts;
+  uint numOfAnswers;
 
   mapping (uint => Post) public posts;
-  mapping (uint => Answer[]) public answers;
+  mapping (uint => Answer) public answers;
   mapping (address => int) balances;
 
   event Transfer(address indexed _from, address indexed _to, int256 _value);
 
   function MetaCoin() {
     numOfPosts = 0;
+    numOfAnswers = 0;
   }
 
-  function sendCoin(address receiver, int amount) returns (bool sufficient) {
+  function sendCoin(address receiver, int amount) returns (bool) {
     balances[msg.sender] -= amount;
     balances[receiver] += amount;
     Transfer(msg.sender, receiver, amount);
     return true;
   }
 
-  function getBalanceInEth(address addr) returns (int) {
+  function getBalanceInEth(address addr) constant returns (int) {
     return ConvertLib.convert(getBalance(addr), 2);
   }
 
-  function getBalance(address addr) public returns (int) {
+  function getBalance(address addr) constant public returns (int) {
     return balances[addr];
   }
 
-  function postQuestion(string title, string content) public returns (uint id) {
-    posts[numOfPosts].id = id;
+  function postQuestion(string title, string content) public returns (bool) {
+    posts[numOfPosts].id = numOfPosts;
     posts[numOfPosts].title = title;
     posts[numOfPosts].content = content;
-    posts[numOfPosts].createdBy = msg.sender;
     posts[numOfPosts].timestamp = now;
-    id = numOfPosts;
+    posts[numOfPosts].createdBy = msg.sender;
     numOfPosts++;
+    return true;
   }
 
-  function getPost(uint postID) public returns (string content, uint timestamp) {
+  function getPost(uint postID) constant public returns (string content, uint timestamp) {
     content = posts[postID].content;
     timestamp = posts[postID].timestamp;
   }
 
-  function getLatestPost() constant returns (string content, uint timestamp, uint numberOfPosts) {
+  function getLatestPost() constant public returns (string content, uint timestamp, uint numberOfPosts) {
     content = posts[numOfPosts - 1].content;
     timestamp = posts[numOfPosts - 1].timestamp;
     numberOfPosts = numOfPosts;
   }
 
-  function getNumberOfPosts() public constant returns (uint num) {
+  function getNumberOfPosts() constant public returns (uint num) {
     num = numOfPosts;
   }
 
-  function postAnswer(uint postId, string answer) returns (uint id) {
+  function postAnswer(uint postId, string answer) public returns (bool) {
+    answers[numOfAnswers].id = numOfAnswers;
+    answers[numOfAnswers].postId = postId;
+    answers[numOfAnswers].content = answer;
+    answers[numOfAnswers].createdBy = msg.sender;
+    answers[numOfAnswers].timestamp = now;
+    numOfAnswers++;
+    return true;
   }
 }
