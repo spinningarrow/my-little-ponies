c73e30e6ffaa111793d9bcea1252980938c6156d
Sahil Bajaj <sabajaj@thoughtworks.com>
2017-11-07 15:04:30 +0800

    Moar style changes

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -94,34 +94,14 @@ window.App = {
 window.addEventListener('load', function () {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn('Using web3 detected from external source. If you find that your accounts don\'t appear or you have 0 MetaCoin, ensure you\'ve configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask')
+    console.warn('Using web3 detected from external source.')
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider)
   } else {
-    console.warn('No web3 detected. Falling back to http://localhost:9545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask')
+    console.warn('No web3 detected. Falling back…')
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8101'))
   }
-
-  const loginForm = document.querySelector('#login')
-
-  loginForm.addEventListener('submit', event => {
-    event.preventDefault()
-
-    const { username, password } = event.target.elements
-
-    // TODO do a proper login
-    if (username.value !== 'alice') {
-      alert('Please enter a valid username. (hint: alice)')
-      return
-    }
-
-    try {
-      web3.personal.unlockAccount(account, password.value)
-    } catch (e) {
-      alert(e)
-    }
-  })
 
   window.App.start()
   ReactDOM.render(h(App, { data }), document.querySelector('#app'))
@@ -171,9 +151,11 @@ const App = ({ data }) => {
 const Header = ({ profile }) => {
   const { username, coins } = profile
   return h('header.site-header', [
-    h(Link, { to: '/' }, 'SiteLogo'),
-    h('div', `Welcome, ${username}!`),
-    h('div', `You have ${coins} coins.`)
+    h(Link, { to: '/', className: '.site-log' }, 'SiteLogo'),
+    h('.profile', [
+      h('span', `Welcome, ${username}!`),
+      h('span', `You have ${coins} coins.`)
+    ])
   ])
 }
 
@@ -216,7 +198,7 @@ const Question = ({ question, hideDetails }) => {
       h('time', moment(timestamp).fromNow()),
       h('span', `${answerCount} ${pluralise('answer', answerCount)}`),
       !hasAcceptedAnswer && answerCount ? h('span.no-accepted-answer', 'no answers accepted') : null,
-      isCurrentUserQuestion && h('span.current-user-question', 'you asked this question')
+      isCurrentUserQuestion && h('span.current-user-question', 'asked by you!')
     ])
   ])
 }
